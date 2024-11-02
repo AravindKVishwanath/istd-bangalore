758bd14d17ab7d8cf46daadf17b2735401f263ba
AravindKVishwanath <aravindkvishwanath59@gmail.com>
2024-11-02 12:35:55 +0530

    other-routes-add-2

---
+++ pages/join/pgdiploma/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../../../components/Navbar';
+
+export default function Join() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Join Us</h1>
+      <p>Become part of our growing community.</p>
+    </div>
+  );
+}
